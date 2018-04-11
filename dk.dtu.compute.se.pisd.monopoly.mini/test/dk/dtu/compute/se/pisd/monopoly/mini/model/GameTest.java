7efe102dce56baef1bfdad729fa8a052f9c52796
Oliver <32456406+outrageousbog@users.noreply.github.com>
2018-04-11 09:30:57 +0200

    Added testdirectory + gameTest

---
+++ dk.dtu.compute.se.pisd.monopoly.mini/test/dk/dtu/compute/se/pisd/monopoly/mini/model/GameTest.java
@@ -0,0 +1,70 @@
+package dk.dtu.compute.se.pisd.monopoly.mini.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSpaces() {
+    }
+
+    @Test
+    void setSpaces() {
+    }
+
+    @Test
+    void addSpace() {
+    }
+
+    @Test
+    void getCardDeck() {
+    }
+
+    @Test
+    void drawCardFromDeck() {
+    }
+
+    @Test
+    void returnCardToDeck() {
+    }
+
+    @Test
+    void setCardDeck() {
+    }
+
+    @Test
+    void shuffleCardDeck() {
+    }
+
+    @Test
+    void getPlayers() {
+    }
+
+    @Test
+    void setPlayers() {
+    }
+
+    @Test
+    void addPlayer() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void setCurrentPlayer() {
+    }
+
+}
