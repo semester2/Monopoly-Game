6632088ec6dde046cef5b4ff1b0d1db87b3f5ccc
Jaafar <jaafarmahdi92@gmail.com>
2018-04-12 10:27:59 +0200

    Overrides computeRent

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/properties/RealEstate.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/properties/RealEstate.java
@@ -1,5 +1,8 @@
 package dk.dtu.compute.se.pisd.monopoly.mini.model.properties;
 
+import java.util.Set;
+
+import dk.dtu.compute.se.pisd.monopoly.mini.model.Player;
 import dk.dtu.compute.se.pisd.monopoly.mini.model.Property;
 
 /**
@@ -12,6 +15,54 @@ import dk.dtu.compute.se.pisd.monopoly.mini.model.Property;
  */
 public class RealEstate extends Property{
 	
-	// TODO to be implemented
-
+	
+	@Override
+	protected int computeRent(Player player) {
+		int initialRent = super.getRent();
+		int color = super.getColorCode();
+		int houses = super.getNumberOfHouses();
+		boolean ownAll = false;
+		boolean twoSpaceColor = false;
+		
+		if (color == super.TWO_SPACE_COLOR_1 || color == super.TWO_SPACE_COLOR_2) {
+			twoSpaceColor = true;
+		}
+		
+		int ownedSameColor = 0;
+		Set<Property> propertyList = player.getOwnedProperties();
+		for (Property property: propertyList) {
+			int propertyColor = property.getColorCode();
+			if (propertyColor == color) {
+				ownedSameColor++;
+			}
+		}
+		
+		if (twoSpaceColor && ownedSameColor == 1) {
+			ownAll = true;
+		} else if (!twoSpaceColor && ownedSameColor == 2) {
+			ownAll = true;
+		}
+		
+		if (ownAll) {
+			initialRent = initialRent * 2;
+		}
+		
+		switch (houses) {
+			case 1: initialRent = initialRent * 2;
+				break;
+			case 2: initialRent = initialRent * 3;
+				break;
+			case 3: initialRent = initialRent * 4;
+				break;
+			case 4: initialRent = initialRent * 5;
+				break;
+			case 5: initialRent = initialRent * 6;
+				break;
+			default: break;
+		}
+		
+		
+		return initialRent;
+	}
+	
 }
