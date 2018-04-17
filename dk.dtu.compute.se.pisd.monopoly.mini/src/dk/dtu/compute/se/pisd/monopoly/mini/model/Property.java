74a12ef4fdf8ade9b9bccea2cb0e266fc5c1564b
sebdtu <32480015+sebdtu@users.noreply.github.com>
2018-04-17 10:02:24 +0200

    mortgageProperty
    with the two overrides in realEstate and Utility

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/Property.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/Property.java
@@ -193,4 +193,7 @@ public class Property extends Space {
 		return this.rent;
 	}
 
+	protected void mortgageProperty(Player player) {
+		
+	}
 }
--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/properties/Utility.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/properties/Utility.java
@@ -1,5 +1,6 @@
 package dk.dtu.compute.se.pisd.monopoly.mini.model.properties;
 
+import dk.dtu.compute.se.pisd.monopoly.mini.model.Player;
 import dk.dtu.compute.se.pisd.monopoly.mini.model.Property;
 
 /**
@@ -12,5 +13,11 @@ import dk.dtu.compute.se.pisd.monopoly.mini.model.Property;
 public class Utility extends Property {
 	
 	// TODO to be implemented
+	
+	@Override
+	protected void mortgageProperty(Player player) {
+		this.setIsMortgaged(true);
+		player.setBalance(player.getBalance()+this.getCost()/2);
+	}
 
 }
