2aea7d5a96fe1a5583552038dbe278cfd82005db
Jaafar <jaafarmahdi92@gmail.com>
2018-04-27 15:42:19 +0200

    Added constructor for RealEstate class

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/properties/RealEstate.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/properties/RealEstate.java
@@ -1,5 +1,7 @@
 package dk.dtu.compute.se.pisd.monopoly.mini.model.properties;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import dk.dtu.compute.se.pisd.monopoly.mini.GameController;
@@ -15,15 +17,34 @@ import dk.dtu.compute.se.pisd.monopoly.mini.model.Property;
  *
  */
 public class RealEstate extends Property implements Comparable<RealEstate> {
-	
+
+	private String color;
 	private int housePrice;
+	private List<Integer> rentRate = new ArrayList<>();
+
+	public RealEstate(int id, String name, String color, int housePrice, int colorCode, int cost, int house0, int house1, int house2, int house3, int house4, int house5) {
+		super.setCost(cost);
+		super.setColorCode(colorCode);
+		super.setIndex(id);
+		super.setName(name);
+		this.color = color;
+		this.housePrice = housePrice;
+		this.rentRate.add(house0);
+		this.rentRate.add(house1);
+		this.rentRate.add(house2);
+		this.rentRate.add(house3);
+		this.rentRate.add(house4);
+		this.rentRate.add(house5);
+	}
 
 	@Override
 	protected int computeRent(Player player, GameController controller) {
 		int rent = super.getRent();
 		boolean ownsAll = controller.checkWetherPlayerOwnAllInColor(player, this.getColorCode());
-		if (ownsAll) {
+		if (ownsAll && this.getNumberOfHouses() == 0) {
 			rent = rent * 2;
+		} else if (ownsAll && this.getNumberOfHouses() > 0) {
+
 		}
 
 		return rent;
