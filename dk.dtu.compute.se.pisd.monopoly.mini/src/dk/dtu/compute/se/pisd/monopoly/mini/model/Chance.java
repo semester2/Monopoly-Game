445606bdcdde81c0dd674704762e40617e5c4218
Jaafar <jaafarmahdi92@gmail.com>
2018-04-27 15:40:59 +0200

    Added constructor for Chance class

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/Chance.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/model/Chance.java
@@ -11,6 +11,24 @@ import dk.dtu.compute.se.pisd.monopoly.mini.model.exceptions.PlayerBrokeExceptio
  */
 public class Chance extends Space {
 
+	private String color;
+
+	/**
+	 * Constructor for Chance objects
+	 * @param id
+	 * @param name
+	 * @param color
+	 * @param colorCode
+	 *
+	 * @author Jaafar Mahdi
+	 */
+	public Chance(int id, String name, String color, int colorCode) {
+		super.setIndex(id);
+		super.setName(name);
+		super.setColorCode(colorCode);
+		this.color = color;
+	}
+
 	@Override
 	public void doAction(GameController controller, Player player) throws PlayerBrokeException {
 		controller.takeChanceCard(player);
