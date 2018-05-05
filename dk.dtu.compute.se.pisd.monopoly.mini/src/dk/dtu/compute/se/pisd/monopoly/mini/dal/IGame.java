21f321e5f12d5293efbc0db4693c0f35bda2fe33
Jaafar <jaafarmahdi92@gmail.com>
2018-05-05 12:02:20 +0200

    Removed unused interface

--- dk.dtu.compute.se.pisd.monopoly.mini/src/dk/dtu/compute/se/pisd/monopoly/mini/dal/IGame.java
+++
@@ -1,9 +0,0 @@
-package dk.dtu.compute.se.pisd.monopoly.mini.dal;
-
-import dk.dtu.compute.se.pisd.monopoly.mini.model.Game;
-
-public interface IGame {
-    boolean createGame(Game game);
-    boolean updateGame(Game game);
-    Game loadGame(int id);
-}
